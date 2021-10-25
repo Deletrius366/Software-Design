411fd520bec0414960e274590563319228ec69e7
Georgiy <you@example.com>
2021-10-25 03:32:49 +0300

    Add tests

---
+++ refactoring/src/main/test/ru/akirakozov/sd/refactoring/servlet/ServletTest.java
@@ -0,0 +1,150 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServletTest {
+
+    private final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+    private final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+
+    private static class Item {
+        String name;
+        int price;
+
+        public Item(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    @BeforeEach
+    private void setup() throws SQLException, IOException {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql1 = "DROP TABLE IF EXISTS PRODUCT";
+            String sql2 = "CREATE TABLE PRODUCT" +
+                    "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " PRICE          INT     NOT NULL)";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql1);
+            stmt.executeUpdate(sql2);
+            stmt.close();
+        }
+        Mockito.when(response.getWriter()).thenReturn(writer);
+    }
+
+    private void addTwoItems () throws IOException {
+        AddProductServlet addProductServlet = new AddProductServlet();
+
+        Mockito.when(request.getParameter("name")).thenReturn("iphone6");
+        Mockito.when(request.getParameter("price")).thenReturn("300");
+        addProductServlet.doGet(request, response);
+
+        Mockito.when(request.getParameter("name")).thenReturn("iphone7");
+        Mockito.when(request.getParameter("price")).thenReturn("400");
+        addProductServlet.doGet(request, response);
+    }
+
+    private int parseQueryResponse(StringWriter stringWriter) {
+        String parsed = stringWriter.toString().split("<html><body>")[1].split(System.lineSeparator())[2];
+        int retVal = 0;
+        try {
+            retVal = Integer.parseInt(parsed);
+        } catch (NumberFormatException e) {
+            retVal = Integer.parseInt(parsed.split("\t")[1].split("</br>")[0]);
+        }
+        return retVal;
+    }
+
+    private List<Item> parseGetProducts(StringWriter stringWriter) {
+        List <Item> items = new ArrayList<>();
+        String [] parsed = stringWriter.toString().split("<html><body>")[1].split(System.lineSeparator());
+        for (String s : parsed) {
+            if (s.isEmpty() || !Character.isLetter(s.charAt(0)))
+                continue;
+            String [] rawItem = s.split("</br>")[0].split("\t");
+            items.add(new Item (rawItem[0], Integer.parseInt(rawItem[1])));
+        }
+        return items;
+    }
+
+    @Test
+    public void QuerySumServletTest() throws IOException {
+        QueryServlet queryServlet = new QueryServlet();
+
+        addTwoItems();
+
+        Mockito.when(request.getParameter("command")).thenReturn("sum");
+        queryServlet.doGet(request, response);
+        Assertions.assertEquals(parseQueryResponse(stringWriter), 700);
+    }
+
+    @Test
+    public void QueryMinServletTest() throws IOException {
+        QueryServlet queryServlet = new QueryServlet();
+
+        addTwoItems();
+
+        Mockito.when(request.getParameter("command")).thenReturn("min");
+        queryServlet.doGet(request, response);
+        Assertions.assertEquals(parseQueryResponse(stringWriter), 300);
+    }
+
+    @Test
+    public void QueryMaxServletTest() throws IOException {
+        QueryServlet queryServlet = new QueryServlet();
+
+        addTwoItems();
+
+        Mockito.when(request.getParameter("command")).thenReturn("max");
+        queryServlet.doGet(request, response);
+        Assertions.assertEquals(parseQueryResponse(stringWriter), 400);
+    }
+
+    @Test
+    public void QueryCountServletTest() throws IOException {
+        QueryServlet queryServlet = new QueryServlet();
+
+        addTwoItems();
+
+        Mockito.when(request.getParameter("command")).thenReturn("count");
+        queryServlet.doGet(request, response);
+        Assertions.assertEquals(parseQueryResponse(stringWriter), 2);
+    }
+
+    @Test
+    public void GetProductServletTest() throws IOException {
+        GetProductsServlet servlet = new GetProductsServlet();
+
+        addTwoItems();
+
+        servlet.doGet(request, response);
+
+        List<Item> items = parseGetProducts(stringWriter);
+
+        Assertions.assertEquals(items.get(0).name, "iphone6");
+        Assertions.assertEquals(items.get(0).price, 300);
+        Assertions.assertEquals(items.get(1).name, "iphone7");
+        Assertions.assertEquals(items.get(1).price, 400);
+    }
+}
