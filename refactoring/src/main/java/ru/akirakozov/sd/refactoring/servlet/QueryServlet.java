4473126a09762b728af4aa6eec7a39dfeefbe57e
Georgiy <you@example.com>
2021-10-25 04:42:03 +0300

    Factor out common code from QueryServlet

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -11,82 +11,46 @@ import java.sql.ResultSet;
  * @author akirakozov
  */
 public class QueryServlet extends AbstractServlet {
+
+    private void doQuery(HttpServletResponse response, DatabaseManager databaseManager, String query, String desc, boolean writeItems) {
+        try {
+            ResultSet rs = databaseManager.executeQuery(query);
+            response.getWriter().println("<html><body>");
+            response.getWriter().println("<h1>" + desc + "</h1>");
+
+            if (writeItems) {
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    int price = rs.getInt("price");
+                    response.getWriter().println(name + "\t" + price + "</br>");
+                }
+            } else {
+                if (rs.next()) {
+                    response.getWriter().println(rs.getInt(1));
+                }
+            }
+            response.getWriter().println("</body></html>");
+
+            rs.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     protected void doGetImpl(HttpServletRequest request, HttpServletResponse response, DatabaseManager databaseManager) throws IOException {
         String command = request.getParameter("command");
 
         if ("max".equals(command)) {
-            try {
-                ResultSet rs = databaseManager.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("<h1>Product with max price: </h1>");
-
-                while (rs.next()) {
-                    String name = rs.getString("name");
-                    int price = rs.getInt("price");
-                    response.getWriter().println(name + "\t" + price + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            doQuery(response, databaseManager, "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1", "Product with max price: ", true);
         } else if ("min".equals(command)) {
-            try {
-                ResultSet rs = databaseManager.executeQuery("SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("<h1>Product with min price: </h1>");
-
-                while (rs.next()) {
-                    String name = rs.getString("name");
-                    int price = rs.getInt("price");
-                    response.getWriter().println(name + "\t" + price + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            doQuery(response, databaseManager, "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1", "Product with min price: ", true);
         } else if ("sum".equals(command)) {
-            try {
-                ResultSet rs = databaseManager.executeQuery("SELECT SUM(price) FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Summary price: ");
-
-                if (rs.next()) {
-                    response.getWriter().println(rs.getInt(1));
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            doQuery(response, databaseManager, "SELECT SUM(price) FROM PRODUCT", "Summary price: ", false);
         } else if ("count".equals(command)) {
-            try {
-                ResultSet rs = databaseManager.executeQuery("SELECT COUNT(*) FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-                response.getWriter().println("Number of products: ");
-
-                if (rs.next()) {
-                    response.getWriter().println(rs.getInt(1));
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            doQuery(response, databaseManager, "SELECT COUNT(*) FROM PRODUCT", "Number of products: ", false);
         } else {
             response.getWriter().println("Unknown command: " + command);
         }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
     }
-
 }
